0cb514aa7819fe47cf74666f006faa412838597c
mamisdra <mamisdra@student.42.fr>
2020-09-14 21:31:53 +0200

    deplacement + basic colision + background

--- player.gd
+++ player.gd
@@ -1,20 +1,22 @@
 extends KinematicBody2D
 
-var speed : int = 200
-var velocity : Vector2 = Vector2()
+var MAX_SPEED = 300
+
+var velocity = Vector2.ZERO
 
 onready var sprite : Sprite = get_node("sprite")
 
 func _physics_process(delta):
-	
-	velocity.x = 0
-	
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= speed
-	if Input.is_action_pressed("move_right"):
-		velocity.x += speed
-	
-	velocity = move_and_slide(velocity, Vector2.UP)
+	var input_vector = Vector2.ZERO    
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector = input_vector.normalized()
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
+	else:
+		velocity = Vector2.ZERO
+		
+	velocity = move_and_slide(velocity * MAX_SPEED)
 	
 	if velocity.x < 0:
 		sprite.flip_h = false
