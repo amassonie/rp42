12890aa30c6ff728fdb61c9a13dbafee8b34ec04
mamisdra <mamisdra@student.42.fr>
2020-09-15 10:50:05 +0200

    animation deplacement

--- player.gd
+++ player.gd
@@ -4,7 +4,9 @@ var MAX_SPEED = 300
 
 var velocity = Vector2.ZERO
 
-onready var sprite : Sprite = get_node("sprite")
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO    
@@ -12,13 +14,11 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = input_vector
 	else:
 		velocity = Vector2.ZERO
-		
+		animationState.travel("Idle")
 	velocity = move_and_slide(velocity * MAX_SPEED)
-	
-	if velocity.x < 0:
-		sprite.flip_h = false
-	elif velocity.x > 0:
-		sprite.flip_h = true
